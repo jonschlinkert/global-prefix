2e2dcdcb88a3274ce619fbc5aa7c1c1efdf62e64
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-05 11:30:00 -0400

    improve tests

--- test.js
+++ test.js
@@ -8,11 +8,23 @@
 'use strict';
 
 require('mocha');
+var path = require('path');
 var assert = require('assert');
+var isWindows = require('is-windows');
 var prefix = require('./');
 
 describe('prefix', function() {
-  it('should:', function() {
-    assert.equal('/usr/local', prefix);
-  });
+  if (isWindows === true || isWindows()) {
+    it('should resolve the path to the windows global prefix:', function() {
+      assert.equal(path.dirname(process.execPath), prefix);
+    });
+  } else {
+    it('should resolve the path to the global prefix:', function() {
+      if (process.env.DESTDIR) {
+        assert.equal(path.join(process.env.DESTDIR, '/usr/local'), prefix);
+      } else {
+        assert.equal('/usr/local', prefix);
+      }
+    });
+  }
 });
